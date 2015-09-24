70695fe71f87762fc71b4c737be3317c0c2adbdc
Nathaniel Kitzke <nat.kitzke@gmail.com>
2015-09-24 18:26:05 +0000

    Resolved the BUG mentionned by ZIAD

--- getFirstChar.js
+++ getFirstChar.js
@@ -1,40 +1,32 @@
 // Exercise-1: Delay LIFTOFF!
 var Promise = require('bluebird');
 
-function delay(ms) {
+function delay(ms, str) {
     return new Promise(function(resolve) {
-        setTimeout(resolve, ms);
+        setTimeout(function() {
+            console.log(str);
+            resolve();
+        }, ms);
     });
 }
 
-delay(1000).then(
+delay(1000, "ONE").then(
     function(value) {
-       console.log("ONE");
-       return delay(1000);
+       return delay(1000, "TWO");
     },
     function(error) {
         console.log(error.message);
     }        
 ).then( 
     function(value) {
-       console.log("TWO");
-       return delay(1000);
+       return delay(1000, "THREE");
     },
     function(error) {
         console.log(error.message);
     }        
 ).then( 
     function(value) {
-       console.log("THREE");
-       return delay(1000);
-    },
-    function(error) {
-        console.log(error.message);
-    }        
-).then( 
-    function(value) {
-       console.log("...LIFTOFF");
-       return delay(1000);
+       return delay(1000, "...LIFTOFF");
     },
     function(error) {
         console.log(error.message);
